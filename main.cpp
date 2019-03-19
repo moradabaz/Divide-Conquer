9ead4bcd153652e2313f6745ea35b6521f2074ca
Morad :) <moradabou1996@gmail.com>
2019-03-19 13:19:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <algorithm>  //for std::generate_n
 #include <map>
 #include <ctime>
+#include <list>
+
 using namespace std;
 
 static const char alfabeto[] = "abcdefghijklmnopqrstuvwxyz";
@@ -15,11 +17,9 @@ static const map<int, int> mapa;
 
 
 
-int ultima_parte_1 = 0;
-int primera_parte_1 = 0;
-int ultima_parte_2 = 0;
-int primera_parte_2 = 0;
-
+list<int> lista;
+int parte_mas_izquierda = 0;
+int parte_mas_derecha = 0;
 
 int combinar(int algoritmo, int algoritmo1);
 
@@ -84,63 +84,103 @@ bool es_combinable(string cadena1, string cadena2) {
     return  (es_vocal(cadena1[cadena1.size() - 1]) && es_vocal(cadena2[0]));
 }
 
+int algoritmo_2(string cadena1, string cadena2);
 
-
-int algoritmo(string cadena1, string cadena2, int tipo) {
+int algoritmo(string cadena1, string cadena2) {
     int longitud = (int) max(cadena2.size(), cadena1.size());
     int accion = 0;
     if (longitud <= 1) return solucion_directa(cadena1, cadena2);
     else {
-        int ultmo_1 = ultima_parte_1;
-        int primero_1 = primera_parte_1;
-
-        int ultimo_2 = ultima_parte_2;
-        int primero_2 = primera_parte_2;
-
         string cadena_media_A_1 = cadena1.substr(0, (unsigned long) (longitud / 2));
         string cadena_media_B_1 = cadena2.substr(0, (unsigned long) (longitud / 2));
         string cadena_media_A_2 = cadena1.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
         string cadena_media_B_2 = cadena2.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
-
-        int res1 = algoritmo(cadena_media_A_1, cadena_media_B_1, tipo);
-        int res2 = algoritmo(cadena_media_A_2, cadena_media_B_2, -tipo);
-
-        int sub_res_1 = 0;
-        int sub_res_2 = 0;
-
-        if (longitud >= 4) {
-            string subcadena_izquierda_A = cadena1.substr((unsigned long) (longitud / 4), (unsigned long) (longitud / 4));
-            string subcadena_derecha_A = cadena1.substr((unsigned long) (longitud / 2), (unsigned long) ( (longitud / 4)));
-
-            string subcadena_izquierda_B = cadena2.substr((unsigned long) (longitud / 4), (unsigned long) (longitud / 4));
-            string subcadena_derecha_B = cadena2.substr((unsigned long) (longitud / 2), (unsigned long) ((longitud / 4)));
-
-            sub_res_1 = algoritmo(subcadena_izquierda_A, subcadena_izquierda_B, 1);
-            sub_res_2 = algoritmo(subcadena_derecha_A, subcadena_derecha_B, 1);
-        }
-
-        cout << "Longitud: " << longitud << endl;
-        cout << "-- res1: " << res1 << endl;
-        cout << "-- res2: " << res2 << endl;
-        cout << "---- subres1: " << sub_res_1 << endl;
-        cout << "---- subres2: "  << sub_res_2 << endl;
-        int resultadofinal = 0;
-
+        int res1 = algoritmo(cadena_media_A_1, cadena_media_B_1);     // Parte 1 calculad
+        int res2_principal = algoritmo(cadena_media_A_2, cadena_media_B_2);
         if(es_combinable(cadena_media_A_1, cadena_media_A_2)) {
-
-
-            int res_alternativo = sub_res_1 + sub_res_2;
-            resultadofinal = res1 + res2;
-            cout << "Resultado alternativo: " << res_alternativo << endl;
-            if (res_alternativo > resultadofinal)
-                return res_alternativo;
-            else
-                return resultadofinal;
+            return res1 + res2_principal;
         } else {
-            resultadofinal = max(res1, res2);
+            return max(res1, res2_principal);
         }
+    }
+}
 
-        return resultadofinal;
+bool es_combinable_() {
+
+}
+
+int algoritmo_1(string cadena1, string cadena2) {
+    int resultado = 0;
+    int longitud = (int) cadena1.size();
+    if (longitud <= 1) return solucion_directa(cadena1, cadena2);
+
+    string cadena_media_A_1 = cadena1.substr(0, (unsigned long) (longitud / 2));
+    string cadena_media_B_1 = cadena2.substr(0, (unsigned long) (longitud / 2));
+    string cadena_media_A_2 = cadena1.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
+    string cadena_media_B_2 = cadena2.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
+
+    int res2 = algoritmo_1(cadena_media_A_2, cadena_media_B_2);
+
+    if (es_combinable(cadena_media_A_1, cadena_media_A_2)) {
+        if (parte_mas_derecha == 0) {
+            int res1 = algoritmo_1(cadena_media_A_1, cadena_media_B_1);
+            return res1 + res2;
+        } else {
+            return res2;
+        }
+    } else {
+        parte_mas_derecha = 1;
+        return res2;
+    }
+}
+
+int algoritmo_2(string cadena1, string cadena2) {
+    int resultado = 0;
+    int longitud = (int) cadena1.size();
+    if (longitud <= 1) return solucion_directa(cadena1, cadena2);
+
+    string cadena_media_A_1 = cadena1.substr(0, (unsigned long) (longitud / 2));
+    string cadena_media_B_1 = cadena2.substr(0, (unsigned long) (longitud / 2));
+    string cadena_media_A_2 = cadena1.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
+    string cadena_media_B_2 = cadena2.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
+
+    int res1 = algoritmo_2(cadena_media_A_1, cadena_media_B_1);     // Parte 1 calculada
+    if (es_combinable(cadena_media_A_1, cadena_media_A_2)) {
+        if (parte_mas_izquierda == 0) {
+            int res2 = algoritmo_2(cadena_media_A_2, cadena_media_B_2);
+
+            return res1 + res2;
+        } else {
+            return res1;
+        }
+    } else {
+        parte_mas_izquierda = 1;
+        return res1;
+    }
+}
+
+int algoritmo_general(string cadena1, string cadena2) {
+    int resultado = 0;
+    int longitud = (int) cadena1.size();
+    if (longitud <= 1) return solucion_directa(cadena1, cadena2);
+
+    string cadena_media_A_1 = cadena1.substr(0, (unsigned long) (longitud / 2));
+    string cadena_media_B_1 = cadena2.substr(0, (unsigned long) (longitud / 2));
+    string cadena_media_A_2 = cadena1.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
+    string cadena_media_B_2 = cadena2.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
+
+    int res1 = algoritmo_general(cadena_media_A_1, cadena_media_B_1);
+    int res2 = algoritmo_general(cadena_media_A_2, cadena_media_B_2);
+    if (es_combinable(cadena_media_A_1, cadena_media_A_2) && es_combinable(cadena_media_B_1, cadena_media_B_2)){
+        int res1_derecho = algoritmo_1(cadena_media_A_1, cadena_media_B_1);
+        int res1_izquierdo = algoritmo_2(cadena_media_A_2, cadena_media_B_2);
+        parte_mas_derecha = 0;
+        parte_mas_izquierda = 0;
+        if (res1_derecho + res1_izquierdo > max(res1, res2))
+            return res1_derecho + res1_izquierdo;
+        else max(res1, res2);
+    } else {
+        return max(res1, res2);
     }
 }
 
@@ -151,14 +191,16 @@ int combinar(int algoritmo, int algoritmo1) {
 
 int main() {
 
-    string cadena1 = "abcaeaabbcaeiouaeiouaeioubekaabbcsabzzzyxaaaaa"";
-    string cadena2 = "abcaeaabbcaeiouaeiouaeioubekaabbcsabzzzyxaaaaa";
+    // keeeee ekaakg
+    // keeeee ekaakg
 
+    string cadena1 =  "abcaeaabbcbekaabbcsabzzzyxaaaaa";       // 5 + 4 + 2
+    string cadena2 =  "abcaeaabbcbekaabbcsabzzzyxaraaa";
 
     cout << cadena1 << endl;
     cout << cadena2 << endl;
 
-    cout  << algoritmo(cadena1, cadena2, 1) << endl;
+    cout  << algoritmo_general(cadena1, cadena2) << endl;
 
     return 0;
 }
