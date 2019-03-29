f48fa960dd8d2dc641bc555448d607cf08f43039
Morad :) <moradabou1996@gmail.com>
2019-03-29 21:48:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,25 +4,19 @@
 #include <string>
 #include <map>
 #include <list>
-
+#include "main.h"
 using namespace std;
 
 static const char alfabeto[] = "abcdefghijklmnopqrstuvwxyz";
 static const char vocales[] = "aeiou";
-static const map<int, int> mapa;
 // crear los generadores
 
 
-
-list<int> lista;
 int parte_mas_izquierda = 0;
 int parte_mas_derecha = 0;
 
-int combinar(int algoritmo, int algoritmo1);
-/*
 string random_string(size_t length )
 {
-
     srand((unsigned int) time(NULL));
     auto randchar = []() -> char
     {
@@ -33,15 +27,7 @@ string random_string(size_t length )
     std::generate_n( str.begin(), length, randchar );
     return str;
 }
-*/
-int encontrar_ocurrencias(string cadena, char caracter) {
-    int ocurrencias = 0;
-    if (cadena.size() == 1)
-        if (cadena.find(caracter, 0))
-            ocurrencias++;
 
-    return ocurrencias;
-}
 
 bool es_vocal(char caracter) {
     for (int i = 0; i < 5; ++i) {
@@ -51,6 +37,12 @@ bool es_vocal(char caracter) {
     return false;
 }
 
+/**
+ *
+ * @param pos_inicial
+ * @param pos_final
+ * @return Esta funcion devuelve la media entre pos_inicial y pos:final
+ */
 int pos_media(int pos_inicial, int pos_final) {
     int media = (pos_final - pos_inicial) / 2;
     if (media > 0)
@@ -58,7 +50,14 @@ int pos_media(int pos_inicial, int pos_final) {
     return pos_inicial;
 }
 
-
+/**
+ *
+ * @param cadena1 Primera Cadena
+ * @param cadena2 Segunda Cadena
+ * @param pos_inicial Posicion inicial de las cadenas
+ * @param pos_final Posicion final de las cadenas
+ * @return
+ */
 int * solucion_directa(string cadena1, string cadena2, int pos_inicial, int pos_final) {
     int * res = new int[3];
     res[0] = 0;
@@ -74,14 +73,30 @@ int * solucion_directa(string cadena1, string cadena2, int pos_inicial, int pos_
     return res;
 }
 
-
-bool es_combinable(string cadena1, string cadena2) {
+/**
+ * Dadas dos cadenas, se comprueba si la ultima letra de la primera cadena y la primera letra de las segunda cadena
+ * son vocales
+ *
+ * Exemplo: adfasdfa y oiuwerwwe
+ *
+ * @param cadena1
+ * @param cadena2
+ * @return
+ */
+bool hay_union_de_vocales(string cadena1, string cadena2) {
     return  (es_vocal(cadena1[cadena1.size() - 1]) && es_vocal(cadena2[0]));
 }
 
-int * algoritmo_2(string cadena1, string cadena2, int pos_inicial, int pos_final);
 
-int * algoritmo_1(string cadena1, string cadena2, int pos_inicial, int pos_final) {
+/**
+ *
+ * @param cadena1
+ * @param cadena2
+ * @param pos_inicial
+ * @param pos_final
+ * @return
+ */
+int * calcular_parte_derecha(string cadena1, string cadena2, int pos_inicial, int pos_final) {
     int * resultado = new int[3];
     resultado[0] = 0;
     resultado[1] = 0;
@@ -95,29 +110,29 @@ int * algoritmo_1(string cadena1, string cadena2, int pos_inicial, int pos_final
     string cadena_media_A_2 = cadena1.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
     string cadena_media_B_2 = cadena2.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
 
-    int * res2 = algoritmo_1(cadena_media_A_2, cadena_media_B_2, pos_inicial, pos_final);
+    int * segunda_parte = calcular_parte_derecha(cadena_media_A_2, cadena_media_B_2, pos_inicial, pos_final);
 
-    if (es_combinable(cadena_media_A_1, cadena_media_A_2)) {
+    if (hay_union_de_vocales(cadena_media_A_1, cadena_media_A_2)) {
         if (parte_mas_derecha == 0) {
-            int * res1 = algoritmo_1(cadena_media_A_1, cadena_media_B_1, pos_inicial, pos_media);
-            resultado[0] = res1[0] + res2[0];
-            resultado[1] = res1[1];
-            resultado[2] = res2[1];
+            int * primera_parte = calcular_parte_derecha(cadena_media_A_1, cadena_media_B_1, pos_inicial, pos_media);
+            resultado[0] = primera_parte[0] + segunda_parte[0];
+            resultado[1] = primera_parte[1];
+            resultado[2] = segunda_parte[1];
         } else {
-            resultado[0] = res2[0];
-            resultado[1] = res2[1];
-            resultado[2] = res2[2];
+            resultado[0] = segunda_parte[0];
+            resultado[1] = segunda_parte[1];
+            resultado[2] = segunda_parte[2];
         }
     } else {
         parte_mas_derecha = 1;
-        resultado[0] = res2[0];
-        resultado[1] = res2[1];
-        resultado[2] = res2[2];
+        resultado[0] = segunda_parte[0];
+        resultado[1] = segunda_parte[1];
+        resultado[2] = segunda_parte[2];
     }
     return resultado;
 }
 
-int * algoritmo_2(string cadena1, string cadena2, int pos_inicial, int pos_final) {
+int * calcular_parte_izquierda(string cadena1, string cadena2, int pos_inicial, int pos_final) {
     int * resultado = new int[3];
     resultado[0] = 0;
     resultado[1] = 0;
@@ -131,19 +146,17 @@ int * algoritmo_2(string cadena1, string cadena2, int pos_inicial, int pos_final
     string cadena_media_A_2 = cadena1.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
     string cadena_media_B_2 = cadena2.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
 
-    int * res1 = algoritmo_2(cadena_media_A_1, cadena_media_B_1, pos_inicial, pos_final);     // Parte 1 calculada
-    if (es_combinable(cadena_media_A_1, cadena_media_A_2)) {
+    int * res1 = calcular_parte_izquierda(cadena_media_A_1, cadena_media_B_1, pos_inicial, pos_final);     // Parte 1 calculada
+    if (hay_union_de_vocales(cadena_media_A_1, cadena_media_A_2)) {
         if (parte_mas_izquierda == 0) {
-            int * res2 = algoritmo_2(cadena_media_A_2, cadena_media_B_2, pos_media, pos_final);
+            int * res2 = calcular_parte_izquierda(cadena_media_A_2, cadena_media_B_2, pos_media, pos_final);
             resultado[0] = res1[0] + res2[0];
             resultado[1] = res1[1];
             resultado[2] = res2[1];
-            //return res1 + res2;
         } else {
             resultado[0] = res1[0];
             resultado[1] = res1[1];
             resultado[2] = res1[2];
-            //return res1;
         }
     } else {
         parte_mas_izquierda = 1;
@@ -167,51 +180,46 @@ int * algoritmo_general(string cadena1, string cadena2, int pos_inicial, int pos
     string cadena_media_A_2 = cadena1.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
     string cadena_media_B_2 = cadena2.substr((unsigned long) (longitud / 2), (unsigned long) longitud);
 
-    int * res1 = algoritmo_general(cadena_media_A_1, cadena_media_B_1, pos_inicial, pos_medio);
-    int * res2 = algoritmo_general(cadena_media_A_2, cadena_media_B_2, pos_medio, pos_final);
-    if (es_combinable(cadena_media_A_1, cadena_media_A_2) && es_combinable(cadena_media_B_1, cadena_media_B_2)) {
-        int * res1_derecho = algoritmo_1(cadena_media_A_1, cadena_media_B_1, pos_inicial, pos_medio);
-        int * res1_izquierdo = algoritmo_2(cadena_media_A_2, cadena_media_B_2, pos_medio, pos_final);
+    int * parte_derecha = algoritmo_general(cadena_media_A_1, cadena_media_B_1, pos_inicial, pos_medio);
+    int * parte_izquierda = algoritmo_general(cadena_media_A_2, cadena_media_B_2, pos_medio, pos_final);
+    
+    if (hay_union_de_vocales(cadena_media_A_1, cadena_media_A_2) && hay_union_de_vocales(cadena_media_B_1, cadena_media_B_2)) {
+        int * parte_mas_derecho = calcular_parte_derecha(cadena_media_A_1, cadena_media_B_1, pos_inicial, pos_medio);
+        int * parte_mas_izquierdo = calcular_parte_izquierda(cadena_media_A_2, cadena_media_B_2, pos_medio, pos_final);
         parte_mas_derecha = 0;
         parte_mas_izquierda = 0;
-        if (res1_derecho[0] + res1_izquierdo[0] > max(res1[0], res2[0])) {
-            resultado[0] = res1_derecho[0] + res1_izquierdo[0];
-            resultado[1] = res1_derecho[1];
-            resultado[2] = res1_izquierdo[1];
+        if (parte_mas_derecho[0] + parte_mas_izquierdo[0] > max(parte_derecha[0], parte_izquierda[0])) {
+            resultado[0] = parte_mas_derecho[0] + parte_mas_izquierdo[0];
+            resultado[1] = parte_mas_derecho[1];
+            resultado[2] = parte_mas_izquierdo[1];
         } else {
-            if (res1[0] >= res2[0]) {
-                resultado[0] = res1[0];
-                resultado[1] = res1[1];
-                resultado[2] = res2[1];
+            if (parte_derecha[0] >= parte_izquierda[0]) {
+                resultado[0] = parte_derecha[0];
+                resultado[1] = parte_derecha[1];
+                resultado[2] = parte_izquierda[1];
             } else {
-                resultado[0] = res2[0];
-                resultado[1] = res2[1];
-                resultado[2] = res2[1];
+                resultado[0] = parte_izquierda[0];
+                resultado[1] = parte_izquierda[1];
+                resultado[2] = parte_izquierda[1];
             }
         }
     } else {
-        if (res1[0] >= res2[0]) {
-            resultado[0] = res1[0];
-            resultado[1] = res1[1];
-            resultado[2] = res2[1];
+        if (parte_derecha[0] >= parte_izquierda[0]) {
+            resultado[0] = parte_derecha[0];
+            resultado[1] = parte_derecha[1];
+            resultado[2] = parte_izquierda[1];
         } else {
-            resultado[0] = res2[0];
-            resultado[1] = res2[1];
-            resultado[2] = res2[1];
+            resultado[0] = parte_izquierda[0];
+            resultado[1] = parte_izquierda[1];
+            resultado[2] = parte_izquierda[1];
         }
     }
+
     return resultado;
 }
 
-int combinar(int algoritmo, int algoritmo1) {
-    return 0;
-}
-
-
+/*
 int main() {
-
-    // keeeee ekaakg
-    // keeeee ekaakg
 
     string cadena1 =  "eeiioouuaaiiabcaeaabbcbekaabbcaeiouaesabzzzyxaaaaa";       // 5 + 4 + 2
     string cadena2 =  "eeiioouuaaiiabcaeaabbcbekaabbcaeiouaesabzzzyxaaaaa";
@@ -219,7 +227,8 @@ int main() {
     cout << cadena1 << endl;
     cout << cadena2 << endl;
 
-    cout << algoritmo_general(cadena1, cadena2, 0, cadena1.size())[0] << " - " << algoritmo_general(cadena1, cadena2, 0, cadena1.size())[1] << endl;
+    cout << algoritmo_general(cadena1, cadena2, 0, cadena1.size()-1)[0] << " - " << algoritmo_general(cadena1, cadena2, 0, cadena1.size()-1)[1] << endl;
 
     return 0;
 }
+*/
